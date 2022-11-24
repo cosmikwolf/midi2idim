1f8b62e7735109a24153b58e0776a9368d3c3fdf
tenkai <tenkai@kariya.ai>
2022-11-23 18:34:19 -0800

    got jest tests working

---
+++ src/addition.test.ts
@@ -0,0 +1,15 @@
+// requireActual ensures you get the real file
+// instead of an automock
+// we use import type and <typeof ...> to still get types
+import type * as Add from "../addition"
+const { addition } = jest.requireActual<typeof Add>("../addition")
+
+describe("addition function", () => {
+  test("two plus two is four", () => {
+    expect(addition(2, 2)).toBe(4)
+  })
+})
+
+// required with this small example
+// to make the isolatedModules config happy
+export {}
---
+++ src/addition.ts
@@ -0,0 +1,3 @@
+export const addition = (first:number, second:number) => {
+    return first + second
+}
